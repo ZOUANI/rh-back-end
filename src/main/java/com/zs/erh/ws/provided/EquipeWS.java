e9c5b4776d8a51428adb902c19243300eee37fa1
Asmae-Moukhtatif <asmae.mokhtatif@gmail.com>
2021-04-23 00:44:46 +0000

    ASMAE

--- src/main/java/com/zs/erh/ws/provided/EquipeWS.java
+++ src/main/java/com/zs/erh/ws/provided/EquipeWS.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
+@RequestMapping("maneo-rh/equipe/")
 public class EquipeWS {
     @Autowired
     private EquipeServiceImple equipeServiceImple;
