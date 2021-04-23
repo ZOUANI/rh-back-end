2aa0adcd5a15726a8717c4c8e96e3d63797316be
Your Name <you@example.com>
2021-04-23 18:09:29 +0000

    el- mahdi add something

--- src/main/java/com/zs/erh/service/facade/CategorieTacheService.java
+++ src/main/java/com/zs/erh/service/facade/CategorieTacheService.java
@@ -1,10 +1,11 @@
 package com.zs.erh.service.facade;
 
 import com.zs.erh.bean.CategorieGroupeTache;
+import com.zs.erh.bean.CategorieTache;
 
 import java.util.List;
 
 public interface CategorieTacheService {
-    public List<CategorieGroupeTache> findAll();
-    public CategorieGroupeTache findByCode(String code);
+    public List<CategorieTache> findAll();
+    public CategorieTache findByCode(String code);
 }
