3f55a2e7598ed3526e16b5bcc867e4a3b1268d03
abdellatif20000 <abdellatifelhantouti90@gmail.com>
2021-04-24 13:58:00 +0000

    Abdellatif g

---
+++ src/main/java/com/zs/erh/service/imple/EtatProjetServiceImple.java
@@ -0,0 +1,17 @@
+package com.zs.erh.service.imple;
+
+import com.zs.erh.bean.EtatProjet;
+import com.zs.erh.dao.EtatProjetDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+@Service
+public class EtatProjetServiceImple {
+    @Autowired
+private EtatProjetDao etatProjetDao;
+
+    public List<EtatProjet> findAll() {
+        return etatProjetDao.findAll();
+    }
+}
