65657255d3165aff65d7894aedcf51adec7978a3
Your Name <you@example.com>
2021-04-25 16:51:24 +0000

    el-mahdi add something

--- src/main/java/com/zs/erh/ws/provided/CategorieGroupeTacheWS.java
+++ src/main/java/com/zs/erh/ws/provided/CategorieGroupeTacheWS.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("maneo-rh/categorieGroupeTache/")
+@RequestMapping("maneo-rh/categorieGroupeTache")
 public class CategorieGroupeTacheWS {
     @Autowired
     private CategorieGroupeTacheService categorieGroupeTacheService;
