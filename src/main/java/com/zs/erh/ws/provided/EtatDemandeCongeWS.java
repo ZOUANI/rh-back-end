4e7b2f7cd8ef1d15902f601bf9f6ef8f4f7d67f1
admin <naoualoueldelhairech@gmail.com>
2021-05-10 18:24:51 +0000

    **Naoual**

--- src/main/java/com/zs/erh/ws/provided/EtatDemandeCongeWS.java
+++ src/main/java/com/zs/erh/ws/provided/EtatDemandeCongeWS.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Optional;
 
-@Controller
+@RestController
 @RequestMapping("etat-demande-conge/etatdemandeconge")
 public class EtatDemandeCongeWS {
     @GetMapping("/code/ {code}")
