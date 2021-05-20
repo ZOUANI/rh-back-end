3102c161c4b59ad07630f07726a9c02f1b054707
Aymane MALIH <aymane.malih@edu.uca.ma>
2021-05-20 23:50:08 +0200

    Agence + etatAgence  + Ville + Chef Agence

--- src/main/java/com/zs/erh/bean/ChefAgence.java
+++ src/main/java/com/zs/erh/bean/ChefAgence.java
@@ -1,7 +1,48 @@
 package com.zs.erh.bean;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.io.Serializable;
 
 @Entity
-public class ChefAgence {
+public class ChefAgence implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String nom;
+    private String prenom;
+    private String code;
+    private String login;
+    private String password;
+    private String phone;
+    private String email;
+
+
+    private int nbrCnx = 3;
+    private boolean blocked;
+    private boolean mustChangePassword = true;
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof ChefAgence)) {
+            return false;
+        }
+        ChefAgence other = (ChefAgence) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
 }
