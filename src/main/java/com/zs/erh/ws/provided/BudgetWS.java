e1c3c1dc02b5b0f63f610f3ab85efd729987c915
Asmae-Moukhtatif <79838714+Asmae-Moukhtatif@users.noreply.github.com>
2021-05-24 16:45:20 +0100

    budget Ws aded

--- src/main/java/com/zs/erh/ws/provided/BudgetWS.java
+++ src/main/java/com/zs/erh/ws/provided/BudgetWS.java
@@ -2,6 +2,7 @@ package com.zs.erh.ws.provided;
 
 import com.zs.erh.bean.Budget;
 import com.zs.erh.bean.Collaborateur;
+import com.zs.erh.bean.Equipe;
 import com.zs.erh.service.facade.BudgetService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -18,6 +19,11 @@ public class BudgetWS {
     public List<Budget> findAll() {
         return budgetService.findAll();
     }
+    @GetMapping("/code/{code}")
+    public Budget findByCode(@PathVariable String code){
+        return budgetService.findByCode(code);
+    }
+
     @PostMapping("/")
     public Budget save(@RequestBody Budget budget){
         return budgetService.save(budget);
