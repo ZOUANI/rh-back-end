56dd5561e45b7db59e6c443bb3506c56512cdf1d
Asmae-Moukhtatif <79838714+Asmae-Moukhtatif@users.noreply.github.com>
2021-06-12 18:45:21 +0100

    ASMAE

---
+++ src/main/java/com/zs/erh/ws/provided/chef_equipe/EquipeCERest.java
@@ -0,0 +1,44 @@
+package com.zs.erh.ws.provided.chef_equipe;
+
+import com.zs.erh.bean.Equipe;
+import com.zs.erh.service.facade.EquipeService;
+import com.zs.erh.service.vo.EquipeVO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("maneo-rh/chef-equipe/equipe")
+public class EquipeCERest {
+    @Autowired
+    private EquipeService equipeService;
+
+    @GetMapping("/")
+    public List<Equipe> findAll(){
+        return equipeService.findAll();
+    }
+
+    @GetMapping("/code/{code}")
+    public Equipe findByCode(@PathVariable String code){
+        return equipeService.findByCode(code);
+    }
+    @GetMapping("/etatEquipeCode/{code}")
+    public List<Equipe> findByEtatEquipeCode(@PathVariable String code) {
+        return this.equipeService.findByEtatEquipeCode(code);
+    }
+    @GetMapping("/collaborateur/code/{code}")
+    public List<Equipe> findByResponsableCode(@PathVariable String code) {
+        return equipeService.findByResponsableCode(code);
+    }
+    @GetMapping("/agenceChefAgenceCode/{code}")
+    public List<Equipe> findByAgenceChefAgenceCode(@PathVariable String code){
+        return equipeService.findByAgenceChefAgenceCode(code);
+    }
+
+    @PostMapping("/search")
+    public List<Equipe> search(@RequestBody EquipeVO equipeVO){
+        return equipeService.search(equipeVO);
+    }
+
+}
---
+++ src/main/java/com/zs/erh/ws/provided/collaborateur/CollaborateurRest.java
@@ -0,0 +1,46 @@
+package com.zs.erh.ws.provided.collaborateur;
+
+import com.zs.erh.bean.Collaborateur;
+import com.zs.erh.service.facade.CollaborateurService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+
+@RestController
+@RequestMapping(value = "maneo-rh/collaborateur")
+public class CollaborateurRest {
+
+	@GetMapping("/login/{login}")
+	public Collaborateur findByLogin(@PathVariable String login) {
+		return collaborateurService.findByLogin(login);
+	}
+
+
+	@GetMapping("/code/{code}")
+	public Collaborateur findByCode(@PathVariable String code) {
+		return collaborateurService.findByCode(code);
+	}
+
+	@GetMapping("/login/{login}/password/{password}")
+	public Collaborateur findByLoginAndPassword(@PathVariable String login,@PathVariable String password){
+		return collaborateurService.findByLoginAndPassword(login,password);
+	}
+	@PostMapping("/")
+	public Collaborateur sinscrire(@RequestBody Collaborateur collaborateur) {
+		return collaborateurService.saveCollaborateur(collaborateur);
+	}
+	@PostMapping("/signIn")
+	public Collaborateur sauthentifier(@RequestBody Collaborateur collaborateur){
+		return collaborateurService.signIn(collaborateur);
+	}
+	@PostMapping("/connexion")
+	public Collaborateur connexionChefEquipe(@RequestBody Collaborateur collaborateur){
+		return collaborateurService.connexionChefEquipe(collaborateur);
+	}
+		@Autowired
+	 private CollaborateurService collaborateurService;
+
+}
