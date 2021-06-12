2c62daf079b486299195ad1849db55f4f8cc4f5c
aymanemalih <79708345+aymanemalih@users.noreply.github.com>
2021-06-12 21:26:37 +0200

    spring security + roles EntrepriseRest ClientRest VilleRest + BudgetRest

--- src/main/java/com/zs/erh/ws/provided/chef_agence/EtatBudgetCARest.java
+++ src/main/java/com/zs/erh/ws/provided/chef_agence/EtatBudgetCARest.java
@@ -1,4 +1,4 @@
-package com.zs.erh.ws.provided;
+package com.zs.erh.ws.provided.chef_agence;
 
 import com.zs.erh.bean.EtatBudget;
 import com.zs.erh.service.facade.EtatBudgetService;
@@ -10,8 +10,8 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping(value = "maneo-rh/etatBudget")
-public class EtatBudgetWS {
+@RequestMapping(value = "maneo-rh/chef-agence/etatBudget")
+public class EtatBudgetCARest {
     @Autowired
     private EtatBudgetService etatBudgetService;
 
