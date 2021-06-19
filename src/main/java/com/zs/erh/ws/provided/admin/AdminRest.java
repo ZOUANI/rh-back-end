827dce3ea471f4db4327b334d7a27a47dc04ec27
Asmae-Moukhtatif <79838714+Asmae-Moukhtatif@users.noreply.github.com>
2021-06-19 15:36:21 +0100

    ASMAE

--- src/main/java/com/zs/erh/ws/provided/admin/AdminRest.java
+++ src/main/java/com/zs/erh/ws/provided/admin/AdminRest.java
@@ -1,12 +1,11 @@
 package com.zs.erh.ws.provided.admin;
 
 import com.zs.erh.bean.Admin;
+import com.zs.erh.bean.User;
 import com.zs.erh.service.facade.AdminService;
+import com.zs.erh.service.facade.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
 
@@ -16,10 +15,11 @@ public class AdminRest {
 
     @Autowired
     private AdminService adminService;
+    @Autowired
+    private UserService userService;
 
     @GetMapping("/login/{login}")
     public Optional<Admin> findBylogin(@PathVariable String login){
-        return this.adminService.findByLogin(login);
+        return adminService.findByLogin(login);
     }
-
 }
